5edd5b8de90038f260a4ed14b9362603e7c83cb9
Gad-Ongoro <gadongoro1@gmail.com>
2023-10-13 16:06:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+
+function submitData(userName, userEmail){
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            Accept : "application/json"
+        },
+        body: JSON.stringify(
+            {
+                name: userName,
+                email: userEmail
+            }
+        )
+    })
+    .then(res => res.json())
+    .then(function(data){
+        document.querySelector("body").innerHTML = data.id;
+        //console.log(data);
+    })
+    .catch(function(error){
+        document.querySelector("body").innerHTML = error.message;
+    })
+}
+//submitData("Simba", "simba@gmail.com");
